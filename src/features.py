d682b741a8dd44a8f4c5b7048ab93d81f4eedbf6
NabilahMO <nmokunola@icloud.com>
2025-06-21 16:31:11 +0100

    Updated text in features.py

--- src/features.py
+++ src/features.py
@@ -1,24 +1,18 @@
 import numpy as np 
-""" imports NumPy, a numerical library used to do math with arrays and 
-vectors which will be needed for the Euclidean distance calculations """
+""" imports NumPy, needed for calculations """
 
 def euclidean_distance(p1, p2):
     """
     def starts a function. 
-    
     The function euclidean_distance(p1, p2) returns the straight-line 
-    distance between two points in 2D space.  p1 and p2 are two 2D points, 
-    like (x, y). 
-    
-    Each point is converted to a NumPy array labelled 
-    np.array(p1). 
-    
-    By subtracting the vectors, it gives a difference vector. 
-    np.linalg.norm(np.array(p1) - np.array(p2)) is an array formed as a 
-    result of computing np.array(p1) - np.array(p2)).
+    distance between p1(x1, y1) and p2(x2, y2) in 2D space. 
     """
     return np.linalg.norm(np.array(p1) - np.array(p2))
-
+"""
+    Converts both input points to NumPy arrays
+	Subtracts one from the other to get a vector
+	np.linalg.norm function returns the length of that vector (distance)
+"""
 
 def compute_inter_iris_distance(left_mid, right_mid):
     """the function above computes the distance between left and right 
@@ -29,20 +23,19 @@ def compute_inter_iris_distance(left_mid, right_mid):
 
 def compute_iris_diameter(inner, outer):
     """Compute the diameter of an iris from inner and outer landmarks
-    using the prestated formula: 
-    np.linalg.norm(np.array(p1) - np.array(p2))"""
+    using the same prestated formula"""
     return euclidean_distance(inner, outer)
 
 
 def compute_average_diameter(left_inner, left_outer, right_inner, right_outer):
-    """Compute average of both irises' diameters."""
+    """Calsculates the average of both irises' diameters."""
     left_diameter = compute_iris_diameter(left_inner, left_outer)
     right_diameter = compute_iris_diameter(right_inner, right_outer)
     return (left_diameter + right_diameter) / 2
 
 
 def normalise_distance(inter_iris_distance, avg_diameter):
-    """Normalise the inter-iris distance by average diameter."""
+    """Returns a normalised version of the inter-iris distance."""
     if avg_diameter == 0:
         return np.nan  # or raise an exception
     return inter_iris_distance / avg_diameter
@@ -56,3 +49,4 @@ def compute_time_series_std(norm_dist_series):
 def flag_mTBI(std_dev, threshold=0.1):
     """Return True if user is likely to have mTBI based on threshold."""
     return std_dev > threshold
+
